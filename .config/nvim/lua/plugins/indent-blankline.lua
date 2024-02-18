957e5e4b3b6676c24056c1452587ea2b1599ddb0
MaxKiv <maxkivits42@gmail.com>
2024-02-18 16:22:42 +0100

    feat: added indent-blankline nvim

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,3 +1,28 @@
 return {
-  { "lukas-reineke/indent-blankline" }
+  { 
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+      indent = { char = "▏" },
+      scope = { enabled =false, show_start = false, show_end = false },
+      exclude = {
+        buftypes = {
+          "nofile",
+          "terminal",
+        },
+        filetypes = {
+          "help",
+          "startify",
+          "aerial",
+          "alpha",
+          "dashboard",
+          "lazy",
+          "neogitstatus",
+          "NvimTree",
+          "neo-tree",
+          "Trouble",
+        },
+      },
+    }
+  }
 }
