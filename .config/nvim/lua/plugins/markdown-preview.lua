630bbb73e4b43d253a057b7a5e429f877d0412e5
MaxKiv <maxkivits42@gmail.com>
2025-05-05 09:46:30 +0200

    feat(nvim): remove useless preview plugin

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -1,18 +1,18 @@
 return {
-  {
-    'iamcco/markdown-preview.nvim',
-    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    ft = { 'markdown' },
-    build = function()
-      vim.fn['mkdp#util#install']()
-    end,
-
-    keys = {
-      {
-        '<leader>m',
-        '<cmd>MarkdownPreviewToggle<cr>',
-        desc = 'Continue',
-      },
-    },
-  },
+  -- {
+  --   'iamcco/markdown-preview.nvim',
+  --   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  --   ft = { 'markdown' },
+  --   build = function()
+  --     vim.fn['mkdp#util#install']()
+  --   end,
+  --
+  --   keys = {
+  --     {
+  --       '<leader>m',
+  --       '<cmd>MarkdownPreviewToggle<cr>',
+  --       desc = 'Continue',
+  --     },
+  --   },
+  -- },
 }
